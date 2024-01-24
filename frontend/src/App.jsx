a1ebb52150cbd0c4167c17c7a1acd8c9f7b77a2e
Dev Italiya <132957942+DevItaliya22@users.noreply.github.com>
2024-01-24 23:13:21 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,9 @@ import MainPage from './components/MainPage';
 
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/todos");
-        const data = await response.json();
-        setTodos(data.todos);
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-        // Handle error, maybe set an error state
-      }
-    };
-
-    fetchData();
-  },[]); // Empty dependency array ensures this runs only once on mount
-
-
- 
   return (
     <Router>
       <div>
-        {/* Routes */}
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/createTodo" element={<CreateTodo />} />
